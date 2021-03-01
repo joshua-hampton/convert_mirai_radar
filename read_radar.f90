e9de998841114f1e92704e6d88f5791a28d3fd3f
Joshua Hampton [RPG] <joshua.hampton@live.co.uk>
2021-03-01 16:08:06 +0000

    Reduced number of spaces to replace with comma, allows for values between -10 and 10

--- read_radar.f90
+++ read_radar.f90
@@ -6,10 +6,10 @@ parameter (ixyz=201, jxyz=201, kxyz=40)
 real      dbz(ixyz, jxyz, kxyz)
 
 open (unit=21, &
-     &      file='/home/eejmh/Desktop/read_radar_mirai/mirai151123_000000.cappi.dbz', &
+     &      file='/home/eejmh/Desktop/read_radar_mirai/mirai151123_011200.cappi.dbz', &
      &      form='unformatted', &
      &      access='direct',recl=ixyz*4)
-open (unit=22, file='/home/eejmh/Desktop/read_radar_mirai/mirai151123_000000.cappi.dbz.txt')
+open (unit=22, file='/home/eejmh/Desktop/read_radar_mirai/mirai151123_011200.cappi.dbz.txt')
 irec=0
    do k=1,kxyz
       do j=1,jxyz
